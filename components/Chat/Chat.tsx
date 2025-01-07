4707bfc082d5fdaecd5d875169b0c70b8d798bde
Sagar Davara <sagardspeed2@gmail.com>
2025-01-07 16:03:53 +0530

    fix: static res

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -309,9 +309,9 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
     const hostname = window.location.hostname;
     // Split hostname by dots and get first part
     const parts = hostname.split('.');
-    setGpuSlug('wonderful_nobel');
+    setGpuSlug(parts[0]);
 
-    getInstanceDetail('wonderful_nobel');
+    getInstanceDetail(parts[0]);
   }, []);
 
   return (
